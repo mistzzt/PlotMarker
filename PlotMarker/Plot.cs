54f73b594bbc22b13ba67024e71f7ca051ba6cf6
Zt Wang <mymisting@gmail.com>
2016-07-26 01:00:03 +0800

    完成划分区域/记录区域功能

--- PlotMarker/Plot.cs
+++ PlotMarker/Plot.cs
@@ -31,9 +31,66 @@ namespace PlotMarker
 
 		public string Owner { get; set; }
 
+		public List<Cell> Cells { get; set; } = new List<Cell>();
+
 		public void GenerateCells(bool empty = true)
 		{
+			if (empty)
+			{
+				TileHelper.RemoveTiles(X, Y, Width, Height);
+			}
 
+			var style = PlotMarker.Config.PlotStyle;
+			var cellX = CellWidth + style.LineWidth;
+			var cellY = CellHeight + style.LineWidth;
+
+			//draw horizental line
+			for (var y = 0; y < Height; y = y + cellY)
+			{
+				for (var x = 0; x < Width; x++)
+				{
+					for (var t = 0; t < style.LineWidth; t++)
+					{
+						TileHelper.SetTile(X + x, Y + y + t, style.TileId, style.TilePaint);
+						TileHelper.SetWall(X + x, Y + y + t, style.WallId, style.WallPaint);
+					}
+				}
+			}
+
+			//draw vertical line
+			for (var x = 0; x < Width; x = x + cellX)
+			{
+				for (var y = 0; y < Height; y++)
+				{
+					for (var t = 0; t < style.LineWidth; t++)
+					{
+						TileHelper.SetTile(X + x + t, Y + y, style.TileId, style.TilePaint);
+						TileHelper.SetWall(X + x + t, Y + y, style.WallId, style.WallPaint);
+					}
+				}
+			}
+
+			TileHelper.ResetSection(X, Y, Width, Height);
+
+			Cells.Clear();
+			for (var x = 0; x < Width; x = x + cellX)
+			{
+				for (var y = 0; y < Height; y = y + cellY)
+				{
+					var cell = new Cell
+					{
+						Id = Cells.Count,
+						Parent = this,
+						X = X + x + style.LineWidth,
+						Y = Y + y + style.LineWidth,
+						Owner = Owner,
+						AllowedIDs = new List<int>()
+					};
+					//new Rectangle(startX + x + style.LineWidth, startY + y + style.LineWidth, style.RoomWidth, style.RoomHeight)
+					Cells.Add(cell);
+				}
+			}
+			PlotMarker.Plots.AddCells(this);
 		}
 	}
 
--- PlotMarker/PlotManager.cs
+++ PlotMarker/PlotManager.cs
@@ -37,8 +37,8 @@ namespace PlotMarker
 			);
 
 			var cellTable = new SqlTable("Cells",
-				new SqlColumn("Id", MySqlDbType.Int32) { AutoIncrement = true, Primary = true },
-				new SqlColumn("PlotId", MySqlDbType.Int32) {NotNull = true},
+				new SqlColumn("PlotId", MySqlDbType.Int32) { NotNull = true },
+				new SqlColumn("CellId", MySqlDbType.Int32) { NotNull = true },
 				new SqlColumn("X", MySqlDbType.Int32) { Unique = true },
 				new SqlColumn("Y", MySqlDbType.Int32) { Unique = true },
 				new SqlColumn("Owner", MySqlDbType.VarChar, 50),
@@ -61,6 +61,61 @@ namespace PlotMarker
 				Debugger.Break();
 				TShock.Log.Error(ex.ToString());
 			}
+		}
+
+		public void Reload()
+		{
+			Plots.Clear();
+
+			using (var reader = _database.QueryReader("SELECT * FROM Plots WHERE WorldId = @0", Main.worldID.ToString()))
+			{
+				while (reader.Read())
+				{
+					var plot = new Plot
+					{
+						Id = reader.Get<int>("Id"),
+						Name = reader.Get<string>("Name"),
+						X = reader.Get<int>("X"),
+						Y = reader.Get<int>("Y"),
+						Width = reader.Get<int>("Width"),
+						Height = reader.Get<int>("Height"),
+						CellWidth = reader.Get<int>("CellWidth"),
+						CellHeight = reader.Get<int>("CellHeight"),
+						WorldId = Main.worldID.ToString(),
+						Owner = reader.Get<string>("Owner"),
+					};
+					plot.Cells = LoadCells(plot);
+					Plots.Add(plot);
+				}
+			}
+		}
+
+		public List<Cell> LoadCells(Plot parent)
+		{
+			var list = new List<Cell>();
+			using (var reader = _database.QueryReader("SELECT * FROM `cells` WHERE PlotId = @0 ORDER BY `cells`.`CellId` ASC", parent.Id))
+			{
+				while (reader.Read())
+				{
+					list.Add(new Cell
+					{
+						Parent = parent,
+						Id = reader.Get<int>("CellId"),
+						X = reader.Get<int>("X"),
+						Y = reader.Get<int>("Y"),
+						Owner = reader.Get<string>("Owner"),
+						GetTime = reader.Get<DateTime?>("GetTime") ?? default(DateTime)
+					});
+				}
+#if DEBUG
+				for (var i = 0; i < list.Count; i++)
+				{
+					if (list[i].Id != i)
+						Debugger.Break();
+				}
+#endif
+			}
+			return list;
 		}
 
 		public bool AddPlot(int x, int y, int width, int height, string name, string owner, string worldid, Style style)
@@ -120,6 +175,40 @@ namespace PlotMarker
 		public Plot GetPlotByName(string plotname)
 		{
 			return Plots.FirstOrDefault(p => p.Name == plotname && p.WorldId == Main.worldID.ToString());
+		}
+
+		public void AddCells(Plot plot)
+		{
+			_database.Query("DELETE FROM Cells WHERE PlotId = @0", plot.Id);
+			Task.Run(() =>
+			{
+				var stopwatch = new Stopwatch();
+				stopwatch.Start();
+				var count = 0;
+				foreach (var cell in plot.Cells)
+				{
+					AddCell(cell);
+					count++;
+				}
+				stopwatch.Stop();
+				Console.WriteLine("记录完毕. 共有{0}个. ({1}ms)", count.ToString(), stopwatch.ElapsedMilliseconds);
+			});
+		}
+
+		public void AddCell(Cell cell)
+		{
+			try
+			{
+				if (_database.Query(
+					"INSERT INTO Cells (PlotId, CellId, X, Y) VALUES (@0, @1, @2, @3);",
+					cell.Parent.Id, cell.Id, cell.X, cell.Y) == 1)
+					return;
+				throw new Exception("No affected rows.");
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
 		}
 	}
 }
