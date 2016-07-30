0982344d70c0355b807d68209cabfee7504a9363
Zt Wang <mymisting@gmail.com>
2016-07-30 12:00:05 +0800

    更新子模块

--- PlotMarker/PlotManager.cs
+++ PlotMarker/PlotManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using MySql.Data.MySqlClient;
@@ -32,15 +33,14 @@ namespace PlotMarker
 				new SqlColumn("Height", MySqlDbType.Int32),
 				new SqlColumn("CellWidth", MySqlDbType.Int32),
 				new SqlColumn("CellHeight", MySqlDbType.Int32),
-				new SqlColumn("WorldId", MySqlDbType.VarChar, 50) { Unique = true },
+				new SqlColumn("WorldId", MySqlDbType.VarChar, 50),
 				new SqlColumn("Owner", MySqlDbType.VarChar, 50)
 			);
 
 			var cellTable = new SqlTable("Cells",
-				new SqlColumn("PlotId", MySqlDbType.Int32) { NotNull = true },
-				new SqlColumn("CellId", MySqlDbType.Int32) { NotNull = true },
-				new SqlColumn("X", MySqlDbType.Int32) { Unique = true },
-				new SqlColumn("Y", MySqlDbType.Int32) { Unique = true },
+				new SqlColumn("Position", MySqlDbType.VarChar, 5) { Primary = true, Unique = true },
+				new SqlColumn("X", MySqlDbType.Int32),
+				new SqlColumn("Y", MySqlDbType.Int32),
 				new SqlColumn("Owner", MySqlDbType.VarChar, 50),
 				new SqlColumn("UserIds", MySqlDbType.Text),
 				new SqlColumn("GetTime", MySqlDbType.Text)
@@ -97,14 +97,15 @@ namespace PlotMarker
 			{
 				while (reader.Read())
 				{
+					DateTime dt;
 					list.Add(new Cell
 					{
 						Parent = parent,
-						Id = reader.Get<int>("CellId"),
+						Id = GetIndex(parent, reader),
 						X = reader.Get<int>("X"),
 						Y = reader.Get<int>("Y"),
 						Owner = reader.Get<string>("Owner"),
-						GetTime = reader.Get<DateTime?>("GetTime") ?? default(DateTime)
+						GetTime = DateTime.TryParse(reader.Get<string>("GetTime"), out dt) ? dt : default(DateTime)
 					});
 				}
 #if DEBUG
@@ -172,15 +173,10 @@ namespace PlotMarker
 			return false;
 		}
 
-		public Plot GetPlotByName(string plotname)
-		{
-			return Plots.FirstOrDefault(p => p.Name == plotname && p.WorldId == Main.worldID.ToString());
-		}
-
-		public void AddCells(Plot plot)
+		public async void AddCells(Plot plot)
 		{
 			_database.Query("DELETE FROM Cells WHERE PlotId = @0", plot.Id);
-			Task.Run(() =>
+			await Task.Run(() =>
 			{
 				var stopwatch = new Stopwatch();
 				stopwatch.Start();
@@ -200,15 +196,79 @@ namespace PlotMarker
 			try
 			{
 				if (_database.Query(
-					"INSERT INTO Cells (PlotId, CellId, X, Y) VALUES (@0, @1, @2, @3);",
-					cell.Parent.Id, cell.Id, cell.X, cell.Y) == 1)
+					"INSERT INTO Cells (Position, X, Y) VALUES (@0, @1, @2);",
+					string.Concat(cell.Parent.Id, ':', cell.Id), cell.X, cell.Y) == 1)
 					return;
 				throw new Exception("No affected rows.");
 			}
 			catch (Exception e)
 			{
+				TShock.Log.ConsoleError("[PM] Cell数值导入数据库失败. ({0}: {1})", cell.Parent.Name, cell.Id);
+				TShock.Log.Error(e.ToString());
+			}
+		}
+
+		public void ApplyForCell(TSPlayer player, Plot plot)
+		{
+			try
+			{
+				if (plot.Cells.All(c => !string.IsNullOrWhiteSpace(c.Owner)))
+				{
+					player.SendWarningMessage("操作失败. 没有剩余区域了.");
+					return;
+				}
+				Cell cell = null;
+				for (var i = plot.Cells.Count - 1; i > 0; i--)
+				{
+					if (string.IsNullOrWhiteSpace(plot.Cells[i].Owner))
+					{
+						cell = plot.Cells[i];
+					}
+				}
+				Debug.Assert(cell != null, "cell != null");
+				cell.Owner = player.Name;
+				cell.GetTime = DateTime.Now;
+
+				//_database.Query("UPDATE")
+
+			}
+			catch (Exception e)
+			{
 				throw;
 			}
+		}
+
+		public Plot GetPlotByName(string plotname)
+		{
+			return Plots.FirstOrDefault(p => p.Name == plotname && p.WorldId == Main.worldID.ToString());
+		}
+
+		private int GetIndex(Plot plot, QueryResult reader)
+		{
+			var text = reader.Get<string>("Position");
+			if (string.IsNullOrWhiteSpace(text) || text.Length > 5)
+			{
+				throw new Exception($"属地 {plot.Name} 区域某一位置数据无效.");
+			}
+			var args = text.Split(':');
+#if DEBUG
+			int test;
+			if (args.Any(a => a == null) || args.Length != 2 || args.Any(a => !int.TryParse(a, out test)))
+			{
+				Debugger.Break();
+				throw new Exception("fuck you, wrong data(string[])");
+			}
+#endif
+			int plotId, cellId;
+			if (!int.TryParse(args[0], out plotId) || !int.TryParse(args[1], out cellId))
+			{
+				throw new Exception("fuck you, wrong data(int.Parse)");
+			}
+			if (plot.Id != plotId)
+			{
+				throw new Exception("fuck you, wrong data(plot.Id != plotId)");
+			}
+			return cellId;
 		}
 	}
 }
